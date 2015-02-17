0f9fe245faeb5a14fcf91b4577e9232655dddf4e
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-17 18:58:09 +0100

    Better commenting and spacing.

--- run_ccf.sh
+++ run_ccf.sh
@@ -9,6 +9,25 @@
 ##
 ################################################################################
 
+##########################################
+## Checking the number of input arguments
+##########################################
+
+if (( $# != 0 )); then
+    echo -e "\tDo not give command line arguments. Usage: ./run_ccf.sh\n"
+    exit
+fi
+
+######################################
+## If heainit isn't running, start it
+######################################
+
+if (( $(echo $DYLD_LIBRARY_PATH | grep heasoft | wc -l) < 1 )); then
+	. $HEADAS/headas-init.sh
+fi
+
+################################################################################
+
 home_dir=$(ls -d ~)  # the -d flag is extremely important here
 day=$(date +%y%m%d)  # make the date a string and assign it to 'day'
 exe_dir="$home_dir/Dropbox/Research/cross_correlation"
@@ -16,8 +35,8 @@ out_dir="$exe_dir/out_ccf"
 # prefix="P70080"
 # obsID="70080-01-01-02"
 prefix="GX339-BQPO"
-# obsID="95335-01-01-00"
-obsID="95409-01-18-00"
+obsID="95335-01-01-06"
+# obsID="95409-01-18-00"
 
 red_dir="$home_dir/Reduced_data/${prefix}/$obsID"
 # red_dir="$home_dur/Dropbox/Research/sample_data"
@@ -28,8 +47,8 @@ in_file="$red_dir/GTId_eventlist_1.fits"
 
 if [ ! -d "$out_dir" ]; then mkdir -p "$out_dir"; fi
 
-dt=1
-numsec=4
+dt=16
+numsec=64
 testing=0  # 0 for no, 1 for yes
 filtering=0 # 0 for no, 1 for yes
 
@@ -49,11 +68,19 @@ tab_ext="fits"
 ##################
 
 if [ -e "$in_file" ] && [ -e "$bkgd_spec" ]; then
-	time python "$exe_dir"/ccf.py "${in_file}" "${out_file}.${tab_ext}" -b "$bkgd_spec" -n "$numsec" -m "$dt" -t "$testing" -f "$filtering"
+
+	time python "$exe_dir"/ccf.py "${in_file}" "${out_file}.${tab_ext}" \
+		-b "$bkgd_spec" -n "$numsec" -m "$dt" -t "$testing" -f "$filtering"
+
 elif [ -e "$in_file" ]; then
-	time python "$exe_dir"/ccf.py "${in_file}" "${out_file}.${tab_ext}" -n "$numsec" -m "$dt" -t "$testing" -f "$filtering"
+
+	time python "$exe_dir"/ccf.py "${in_file}" "${out_file}.${tab_ext}" \
+		-n "$numsec" -m "$dt" -t "$testing" -f "$filtering"
+
 else
-	echo "$in_file and/or $bkgd_spec does not exist. CCF.py was not run."
+
+	echo -e "\tERROR: ccf.py was not run. Eventlist and/or background energy spectrum doesn't exist."
+
 fi
 
 
@@ -62,12 +89,19 @@ fi
 #################
 
 if [ -e "${out_file}.${tab_ext}" ]; then
-	python "$exe_dir"/plot_ccf.py "${out_file}.${tab_ext}" -o "${plot_root}" -p "${prefix}/${obsID}"
-# 	if [ -e "${plot_root}_chan_06.png" ]; then open -a ImageJ "${plot_root}_chan_06.png"; fi
 
-	ccfs_plot="$exe_dir/${day}_ccfs.png"
-	python "$exe_dir"/plot_multi.py "${out_file}.${tab_ext}" "$ccfs_plot" "${numsec}"
+	python "$exe_dir"/plot_ccf.py "${out_file}.${tab_ext}" -o "${plot_root}" \
+		-p "${prefix}/${obsID}"
+	
+	if [ -e "${plot_root}_chan_06.png" ]; then open -a ImageJ "${plot_root}_chan_06.png"; fi
+
+# 	ccfs_plot="$exe_dir/${day}_ccfs.png"
+
+# 	python "$exe_dir"/plot_multi.py "${out_file}.${tab_ext}" "$ccfs_plot" \
+# 		"${numsec}"
+
 # 	if [ -e "$ccfs_plot" ]; then open -a ImageJ "$ccfs_plot"; fi
+
 fi
 
 
@@ -78,9 +112,28 @@ fi
 # plot_file="${plot_root}_2Dccf.png"
 # if [ -e "${out_file}.${tab_ext}" ]; then
 # 	python "$exe_dir"/plot_2d.py "${out_file}.${tab_ext}" -o "${plot_file}"
+# 	if [ -e "${plot_file}" ]; then open -a ImageJ "${plot_file}"; fi
 # fi
-# if [ -e "${plot_file}" ]; then open -a ImageJ "${plot_file}"; fi
-
+# 
+# plot_file="${plot_root}_2Dccf.fits"
+# 
+# if [ -e "$out_dir/temp.dat" ]; then
+# 	fimgcreate bitpix=-32 \
+# 		naxes="2000,64" \
+# 		datafile="$out_dir/temp.dat" \
+# 		outfile="${plot_root}_2Dccf.fits" \
+# 		nskip=1 \
+# 		history=true \
+# 		clobber=yes
+# else
+# 	echo -e "\tERROR: FIMGCREATE did not run. 2Dccf temp file does not exist."
+# fi
+# 
+# if [ -e "${plot_root}_2Dccf.fits" ]; then
+# 	echo "FITS 2D ccf ratio image: ${plot_root}_2Dccf.fits"
+# else
+# 	echo -e "\tERROR: FIMGCREATE was not successful."
+# fi
 
 #####################
 ## Plotting the lags
@@ -99,10 +152,9 @@ fi
 # fi
 # 
 # if [ -e "${out_file}.${tab_ext}" ]; then
-# # 	echo python "$lag_exe_dir"/plot_lags.py "${out_file}.${tab_ext}" -o "${plot_root}"	
 # 	python "$lag_exe_dir"/plot_lags.py "${out_file}.${tab_ext}" -o "${plot_root}"	
 # else
-# 	echo "${out_file}.${tab_ext} does not exist. plot_lags.py was not run."
+# 	echo -e "\tERROR: plot_lags.py was not run. Lag output file does not exist."
 # fi
 
 
--- run_multi_ccf.sh
+++ run_multi_ccf.sh
@@ -1,88 +1,155 @@
 #!/bin/bash
 
-## Script to run multi_CCF.py and plot_CCF.py
+################################################################################
+## 
+## Bash script to run multi_CCF.py, plot_CCF.py, plot_multi.py, plot_2d.py, and 
+## plot_lags.py
+##
+## Written by Abigail Stevens, A.L.Stevens@uva.nl, 2014-2015
+##
+################################################################################
 
-home_dir=$(ls -d ~)  ## the -d flag is extremely important here
+##########################################
+## Checking the number of input arguments
+##########################################
 
-exe_dir="$home_dir/Dropbox/Research/cross_correlation"
-out_dir="$exe_dir/out_ccf" # for multiple input files with different obsIDs
-day=$(date +%y%m%d)  ## make the date a string and assign it to 'day'
-# day="141215"
-
-
-if [ ! -d "$out_dir" ]; then 
-	mkdir -p "$out_dir"
+if (( $# != 6 )); then
+    echo -e "\tUsage: ./run_multi_ccf.sh <file list> <prefix> <dt multiple> <num seconds> <testing> <date>\n"
+    exit
 fi
 
 file_list=$1
-propID=$2
+prefix=$2
 dt=$3
 numsec=$4
 testing=$5
+day=$6
+
+######################################
+## If heainit isn't running, start it
+######################################
+
+if (( $(echo $DYLD_LIBRARY_PATH | grep heasoft | wc -l) < 1 )); then
+	. $HEADAS/headas-init.sh
+fi
+
+################################################################################
+
+home_dir=$(ls -d ~)
+
+exe_dir="$home_dir/Dropbox/Research/cross_correlation"
+out_dir="$exe_dir/out_ccf" # for multiple input files with different obsIDs
+
+if [ ! -d "$out_dir" ]; then mkdir -p "$out_dir"; fi
+
+filtering=0
+# bkgd_spec="$home_dir/Reduced_data/$prefix/evt_bkgd_rebinned.pha"
+bkgd_spec="$home_dir/Dropbox/Research/sample_data/evt_bkgd_rebinned.pha"
+
 
 tab_ext="fits"
 plot_ext="png"
 
 if (( $testing == 0 )); then
-	out_file="$out_dir/${propID}_${day}_t${dt}_${numsec}sec"
-	plot_root="$out_dir/${propID}_${day}_t${dt}_${numsec}sec"
-	saved_file_list="$out_dir/${propID}_${day}_t${dt}_${numsec}sec_filelist"
+	out_file="$out_dir/${prefix}_${day}_t${dt}_${numsec}sec"
+	plot_root="$out_dir/${prefix}_${day}_t${dt}_${numsec}sec"
+	saved_file_list="$out_dir/${prefix}_${day}_t${dt}_${numsec}sec_filelist"
 elif (( $testing == 1 )); then
-	out_file="$out_dir/test_${propID}_${day}_t${dt}_${numsec}sec"
-	plot_root="$out_dir/test_${propID}_${day}_t${dt}_${numsec}sec"
-	saved_file_list="$out_dir/test_${propID}_${day}_t${dt}_${numsec}sec_filelist"
+	out_file="$out_dir/test_${prefix}_${day}_t${dt}_${numsec}sec"
+	plot_root="$out_dir/test_${prefix}_${day}_t${dt}_${numsec}sec"
+	saved_file_list="$out_dir/test_${prefix}_${day}_t${dt}_${numsec}sec_filelist"
 fi
 
 cp "$file_list" "$saved_file_list"
 
-if [ -e "$saved_file_list" ]; then
-	python "$exe_dir"/multi_CCF.py -i "$saved_file_list" -o "${out_file}.${tab_ext}" -n "$numsec" -m "$dt" -t "$testing"
+########################
+## Running multi_ccf.py
+########################
+
+if [ -e "$saved_file_list" ] && [ -e "$bkgd_spec" ]; then
+
+	python "$exe_dir"/multi_CCF.py "$saved_file_list" "${out_file}.${tab_ext}" \
+		-b "$bkgd_spec" -n "$numsec" -m "$dt" -t "$testing" -f "$filtering"
+		
+elif [ -e "$saved_file_list" ]; then
+
+	python "$exe_dir"/multi_CCF.py "$saved_file_list" "${out_file}.${tab_ext}" \
+		-n "$numsec" -m "$dt" -t "$testing" -f "$filtering"
+
+else 
+	
+	echo -e "\tERROR: multi_ccf.py was not run. List of eventlists and/or background energy spectrum doesn't exist."
+
 fi
+
+####################
+## Plotting the ccf
+####################
 
 if [ -e "${out_file}.${tab_ext}" ]; then
-	python "$exe_dir"/plot_CCF.py "${out_file}.${tab_ext}" -o "${plot_root}" -p "$propID"
-	if [ -e "${plot_root}_chan_11.${plot_ext}" ]; then
-		open -a ImageJ "${plot_root}_chan_11.${plot_ext}"
-	fi
+	python "$exe_dir"/plot_CCF.py "${out_file}.${tab_ext}" -o "${plot_root}" \
+		-p "$prefix"
+# 	if [ -e "${plot_root}_chan_11.${plot_ext}" ]; then open -a ImageJ "${plot_root}_chan_11.${plot_ext}"; fi
 	
 	ccfs_plot="$exe_dir/ccf_plot.${plot_ext}"
-	python "$exe_dir"/plot_multi.py "${out_file}.${tab_ext}" "$ccfs_plot" "$numsec"
-	if [ -e "$ccfs_plot" ]; then
-		open -a ImageJ "$ccfs_plot"
-	fi
+	python "$exe_dir"/plot_multi.py "${out_file}.${tab_ext}" "$ccfs_plot" \
+		"$numsec"
+# 	if [ -e "$ccfs_plot" ]; then open -a ImageJ "$ccfs_plot"; fi
 fi
 
-## Plotting the 2D ccf with colours
+#######################
+## Plotting the 2D ccf
+#######################
 
 plot_file="${plot_root}_2Dccf.${plot_ext}"
 if [ -e "${out_file}.${tab_ext}" ]; then
 	python "$exe_dir"/plot_2d.py "${out_file}.${tab_ext}" -o "${plot_file}"
+# 	if [ -e "${plot_file}" ]; then open -a ImageJ "${plot_file}"; fi
 fi
-if [ -e "${plot_file}" ]; then
-	open -a ImageJ "${plot_file}"
-fi
+	
+plot_file="${plot_root}_2Dccf.fits"
 
+# if [ -e "$out_dir/temp.dat" ]; then
+# 	fimgcreate bitpix=-32 \
+# 		naxes="2000,64" \
+# 		datafile="$out_dir/temp.dat" \
+# 		outfile="${plot_root}_2Dccf.fits" \
+# 		nskip=1 \
+# 		history=true \
+# 		clobber=yes
+# else
+# 	echo -e "\tERROR: FIMGCREATE did not run. 2Dccf temp file does not exist."
+# fi
+# 
+# if [ -e "${plot_root}_2Dccf.fits" ]; then
+# 	echo "FITS 2D ccf ratio image: ${plot_root}_2Dccf.fits"
+# else
+# 	echo -e "\tERROR: FIMGCREATE was not successful."
+# fi
 
+#####################
 ## Plotting the lags
+#####################
 
 lag_exe_dir="$home_dir/Dropbox/Research/lags"
 lag_out_dir="$lag_exe_dir/out_lags"
 cd "$lag_exe_dir"
 
 if (( $testing == 0 )); then
-	out_file="$lag_out_dir/${propID}_${day}_t${dt}_${numsec}sec"
-	plot_root="$lag_out_dir/${propID}_${day}_t${dt}_${numsec}sec"
+	out_file="$lag_out_dir/${prefix}_${day}_t${dt}_${numsec}sec"
+	plot_root="$lag_out_dir/${prefix}_${day}_t${dt}_${numsec}sec"
 elif (( $testing == 1 )); then
-	out_file="$lag_out_dir/test_${propID}_${day}_t${dt}_${numsec}sec"
-	plot_root="$lag_out_dir/test_${propID}_${day}_t${dt}_${numsec}sec"
+	out_file="$lag_out_dir/test_${prefix}_${day}_t${dt}_${numsec}sec"
+	plot_root="$lag_out_dir/test_${prefix}_${day}_t${dt}_${numsec}sec"
 fi
 
 if [ -e "${out_file}.${tab_ext}" ]; then
-# 	echo python "$lag_exe_dir"/plot_lags.py "${out_file}.${tab_ext}" -o "${plot_root}"	
 	python "$lag_exe_dir"/plot_lags.py "${out_file}.${tab_ext}" -o "${plot_root}"
+# 	if [ -e "${plot_root}_lag-energy.png" ]; then open -a ImageJ "${plot_root}_lag-energy.png"; fi
 else
-	echo "${out_file}.${tab_ext} does not exist. plot_lags.py was not run."
+	echo -e "\tERROR: plot_lags.py was not run. Lag output file does not exist."
 fi
-# if [ -e "${plot_root}_lag-energy.png" ]; then
-# 	open -a ImageJ "${plot_root}_lag-energy.png"
-# fi
+
+################################################################################
+## All done!
+################################################################################
