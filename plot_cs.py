0e43c7e331d1d69ec4d09e3e48350b89585f6ff8
Abigail Stevens <A.L.Stevens@uva.nl>
2015-02-23 20:06:10 +0100

    Made slightly more useful. Now has frequency instead of just bins.

--- plot_cs.py
+++ plot_cs.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 
 table = np.loadtxt('cs_avg.dat')
 
-cs = np.asarray(table[:,0])
-fig = plt.subplots()
-bins = np.arange(len(cs))
-plt.plot(bins, cs)
+freq = np.asarray(table[:,0])
+cs = np.asarray(table[:,1])
+fig,ax = plt.subplots(1,1,figsize=(6,6))
+ax.plot(freq, cs)
+ax.set_xlim(0,20)
 plt.show()
 
