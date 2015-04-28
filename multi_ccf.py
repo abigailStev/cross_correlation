caa2348039ad30880fbc271e4cce09228b5e6995
Abigail Stevens <A.L.Stevens@uva.nl>
2015-04-29 00:29:14 +0200

    Working on docstring comments, reformatted argparse strings to look pretty in the code, updated some comments.

--- multi_ccf.py
+++ multi_ccf.py
@@ -26,6 +26,45 @@ def dat_out(out_file, in_file_list, bkgd_file, param_dict, mean_rate_ci_total, \
     """
     Writes the cross-correlation function to a .dat output file.
 
+    Parameters
+    ----------
+    out_file : string
+        Description.
+
+    in_file_list : string
+        Description.
+
+    out_file : string
+        Description.
+
+    bkgd_file : string
+        Description.
+
+    param_dict : dict
+        Description.
+
+    mean_rate_ci_total : np.array of floats
+        Description.
+
+    mean_rate_ref_total : float
+        Description.
+
+    t : np.array of ints
+        Description.
+
+    ccf : np.array of floats
+        Description.
+
+    ccf_error : np.array of floats
+        Description.
+
+    filtering : boolean
+        Description.
+
+
+    Returns
+    -------
+    nothing
     """
     if out_file[-4:].lower() == "fits":
         out_file = out_file[:-4]+"dat"
@@ -71,6 +110,46 @@ def fits_out(out_file, in_file_list, bkgd_file, param_dict, mean_rate_ci_total,\
     mean_rate_ref_total, t, ccf, ccf_error, filtering):
     """
     Writes the cross-correlation function to a .fits output file.
+
+    Parameters
+    ----------
+    out_file : string
+        Description.
+
+    in_file_list : string
+        Description.
+
+    out_file : string
+        Description.
+
+    bkgd_file : string
+        Description.
+
+    param_dict : dict
+        Description.
+
+    mean_rate_ci_total : np.array of floats
+        Description.
+
+    mean_rate_ref_total : float
+        Description.
+
+    t : np.array of ints
+        Description.
+
+    ccf : np.array of floats
+        Description.
+
+    ccf_error : np.array of floats
+        Description.
+
+    filtering : boolean
+        Description.
+
+
+    Returns
+    -------
+    nothing
 
     """
     print "\nOutput sent to: %s" % out_file
@@ -135,6 +214,34 @@ def main(in_file_list, out_file, bkgd_file, num_seconds, dt_mult, test,
     the segments per energy channel, and then computes the cross-correlation
     function (ccf) per energy channel.
 
+    Parameters
+    ----------
+    in_file_list : string
+        Description.
+
+    out_file : string
+        Description.
+
+    bkgd_file : string
+        Description.
+
+    num_seconds : int
+        Description.
+
+    dt_mult : int
+        Description.
+
+    test : boolean
+        Description.
+
+    filtering : boolean
+        Description.
+
+
+    Returns
+    -------
+    nothing
+
     """
 
     #####################################################
@@ -172,11 +279,13 @@ def main(in_file_list, out_file, bkgd_file, num_seconds, dt_mult, test,
                  'detchans': detchans}
 
     total_seg = 0
-    cs_sum_total = np.zeros((n_bins, detchans), dtype=np.complex128)
-    sum_rate_ci_total = np.zeros(detchans)
+    cs_sum_total = np.zeros((param_dict['n_bins'], param_dict['detchans']), \
+        dtype=np.complex128)
+    sum_rate_ci_total = np.zeros(param_dict['detchans'])
     sum_rate_ref_total = 0
-    sum_power_ci_total = np.zeros((n_bins, detchans), dtype=np.float64)
-    sum_power_ref_total = np.zeros(n_bins, dtype=np.float64)
+    sum_power_ci_total = np.zeros((param_dict['n_bins'], \
+        param_dict['detchans']), dtype=np.float64)
+    sum_power_ref_total = np.zeros(param_dict['n_bins'], dtype=np.float64)
 
     print "\nDT = %.15f" % param_dict['dt']
     print "N_bins = %d" % param_dict['n_bins']
@@ -191,7 +300,7 @@ def main(in_file_list, out_file, bkgd_file, num_seconds, dt_mult, test,
         print "Using background spectrum: %s" % bkgd_file
         bkgd_rate = xcor.get_background(bkgd_file)
     else:
-        bkgd_rate = np.zeros(detchans)
+        bkgd_rate = np.zeros(param_dict['detchans'])
     print " "
 
     ##################################
@@ -260,10 +369,6 @@ def main(in_file_list, out_file, bkgd_file, num_seconds, dt_mult, test,
     xcor.save_for_lags(out_file, in_file_list, param_dict, mean_rate_ci_total,
         mean_rate_ref_total, cs_avg, mean_power_ci, mean_power_ref)
 
-# 	xcor.make_lags(out_file, in_file_list, dt, n_bins, detchans, num_seconds,
-# 		total_seg, mean_rate_ci_total, mean_rate_ref_total, cs_avg,
-# 		mean_power_ci, mean_power_ref)
-
     ##############################################
     ## Computing ccf from cs, and computing error
     ##############################################
@@ -289,9 +394,6 @@ def main(in_file_list, out_file, bkgd_file, num_seconds, dt_mult, test,
     ## Output
     ##########
 
-#     dat_out(out_file, in_file_list, bkgd_file, param_dict, mean_rate_ci_total,
-#       mean_rate_ref_total, t, ccf_end, ccf_error, filtering)
-
     fits_out(out_file, in_file_list, bkgd_file, param_dict, mean_rate_ci_total,\
         mean_rate_ref_total, t, ccf_end, ccf_error, filtering)
 
@@ -303,36 +405,41 @@ if __name__ == "__main__":
     ## Parsing input arguments and calling 'main'
     ##############################################
 
-    parser = argparse.ArgumentParser(usage="python multi_ccf.py infile outfile \
-[-b BKGD_SPECTRUM] [-n NUM_SECONDS] [-m DT_MULT] [-t {0,1}] [-f {0,1}]",
-description='Computes the cross-correlation function of a channel of interest \
-with a reference band, over multiple RXTE eventlists.', epilog='For optional \
-arguments, default values are given in brackets at end of description.')
+    parser = argparse.ArgumentParser(usage="python multi_ccf.py infile outfile"\
+        " [-b BKGD_SPECTRUM] [-n NUM_SECONDS] [-m DT_MULT] [-t {0,1}] [-f "\
+        "{0,1}]", description="Computes the cross-correlation function of a "\
+        "channel of interest with a reference band, over multiple RXTE "\
+        "eventlists.", epilog="For optional arguments, default values are "\
+        "given in brackets at end of description.")
 
-    parser.add_argument('infile_list', help="The name of the (ASCII/txt/\
-dat) input file listing the event lists to be used. One file per line.")
+    parser.add_argument('infile_list', help="The name of the ASCII "\
+        "file listing the event lists to be used. One file per line. Each "\
+        "event list must be .fits or .dat format containing both the reference"\
+        " band and the channels of interest. Assumes channels of interest = "\
+        "PCU 2, ref band = all other PCUs.")
 
-    parser.add_argument('outfile', help="The name of the FITS file to write the\
- cross-correlation function to.")
+    parser.add_argument('outfile', help="The name of the FITS file to write "\
+        "the cross-correlation function to.")
 
     parser.add_argument('-b', '--bkgd', required=False, dest='bkgd_file',
-help="Name of the (pha/fits) background spectrum. [none]")
+        help="Name of the (pha/fits) background spectrum. [none]")
 
     parser.add_argument('-n', '--num_seconds', type=tools.type_power_of_two,
-default=1, dest='num_seconds', help="Number of seconds in each Fourier segment.\
- Must be a power of 2, positive, integer. [1]")
+        default=1, dest='num_seconds', help="Number of seconds in each Fourier"\
+        " segment. Must be a power of 2, positive, integer. [1]")
 
     parser.add_argument('-m', '--dt_mult', type=tools.type_power_of_two,
-default=1, dest='dt_mult', help="Multiple of dt (dt is from data file) for \
-timestep between bins. Must be a power of 2, positive, integer. [1]")
+        default=1, dest='dt_mult', help="Multiple of dt (dt is from data file)"\
+        " for timestep between bins. Must be a power of 2, positive, integer. "\
+        "[1]")
 
     parser.add_argument('-t', '--test', type=int, default=0, choices={0,1},
-dest='test', help="Int flag: 0 if computing all segments, 1 if computing only \
-one segment for testing. [0]")
+        dest='test', help="Int flag: 0 if computing all segments, 1 if "\
+        "computing only one segment for testing. [0]")
 
     parser.add_argument('-f', '--filter', type=int, default=0, choices={0,1},
-dest='filter', help='Int flag: 0 if NOT applying a filter in frequency-space, \
-1 if applying filter (around a coherent pulsation). [0]')
+        dest='filter', help="Int flag: 0 if NOT applying a filter in frequency"\
+        "-space, 1 if applying filter (around a coherent pulsation). [0]")
 
     args = parser.parse_args()
 
