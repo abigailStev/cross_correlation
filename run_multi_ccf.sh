14db15eb13f51211e471ee416af1d0e0e1f39448
Abigail Stevens <A.L.Stevens@uva.nl>
2015-09-01 17:01:23 +0200

    Commenting and uncommenting parts.

--- run_multi_ccf.sh
+++ run_multi_ccf.sh
@@ -42,7 +42,7 @@ fi
 home_dir=$(ls -d ~)
 
 exe_dir="$home_dir/Dropbox/Research/cross_correlation"
-out_dir="$exe_dir/out_ccf"
+out_dir="$exe_dir/out_ccf/${prefix}"
 lag_exe_dir="$home_dir/Dropbox/Research/lags"
 lag_out_dir="$lag_exe_dir/out_lags"
 xte_exe_dir="$home_dir/Dropbox/Research/rxte_reduce"
@@ -57,8 +57,8 @@ if [ ! -d "$lag_out_dir" ]; then mkdir -p "$lag_out_dir"; fi
 
 bkgd_spec="$home_dir/Reduced_data/$prefix/evt_bkgd_rebinned.pha"
 
-#filename="${prefix}_${day}_t${dt}_${numsec}sec_adj"
-filename="${prefix}_${day}_t${dt}_${numsec}sec"
+filename="${prefix}_${day}_t${dt}_${numsec}sec_adj"
+#filename="${prefix}_${day}_t${dt}_${numsec}sec"
 
 lag_lf=4.0  ## Lower frequency bound for lag spectra, in Hz
 lag_uf=7.0  ## Upper frequency bound for lag spectra, in Hz
@@ -95,10 +95,10 @@ cp "$file_list" "$saved_file_list"
 ########################
 
 if [ -e "$saved_file_list" ] && [ -e "$bkgd_spec" ]; then
-	python "$exe_dir"/multi_CCF.py "$saved_file_list" "${out_file}.${t_ext}" \
+	python "$exe_dir"/multi_ccf.py "$saved_file_list" "${out_file}.${t_ext}" \
 		-b "$bkgd_spec" -n "$numsec" -m "$dt" -t "$testing" -f "$filtering" -a
 elif [ -e "$saved_file_list" ]; then
-	python "$exe_dir"/multi_CCF.py "$saved_file_list" "${out_file}.${t_ext}" \
+	python "$exe_dir"/multi_ccf.py "$saved_file_list" "${out_file}.${t_ext}" \
 		-n "$numsec" -m "$dt" -t "$testing" -f "$filtering" -a
 else
 	echo -e "\tERROR: multi_ccf.py was not run. List of eventlists and/or "\
@@ -109,16 +109,16 @@ fi
 ## Plotting the individual ccfs
 ################################
 
-if [ -e "${out_file}.${t_ext}" ]; then
-	python "$exe_dir"/plot_CCF.py "${out_file}.${t_ext}" -o "${plot_root}" \
-		-p "$prefix" --ext "$p_ext"
-#	if [ -e "${plot_root}_chan_15.${p_ext}" ]; then open "${plot_root}_chan_15.${p_ext}"; fi
-
-	multi_plot="${plot_root}_multiccfs.${p_ext}"
-	python "$exe_dir"/plot_multi.py "${out_file}.${t_ext}" "$multi_plot" \
-		-p "$prefix"
-#	if [ -e "$multi_plot" ]; then open "$multi_plot"; fi
-fi
+#if [ -e "${out_file}.${t_ext}" ]; then
+#	python "$exe_dir"/plot_CCF.py "${out_file}.${t_ext}" -o "${plot_root}" \
+#		-p "$prefix" --ext "$p_ext"
+##	if [ -e "${plot_root}_chan_15.${p_ext}" ]; then open "${plot_root}_chan_15.${p_ext}"; fi
+#
+#	multi_plot="${plot_root}_multiccfs.${p_ext}"
+#	python "$exe_dir"/plot_multi.py "${out_file}.${t_ext}" "$multi_plot" \
+#		-p "$prefix"
+##	if [ -e "$multi_plot" ]; then open "$multi_plot"; fi
+#fi
 
 ###############################################
 ## Getting the energy list from a channel list
@@ -139,37 +139,37 @@ fi
 ## Plotting the 2D ccf
 #######################
 
-plot_file="${plot_root}_2Dccf.${p_ext}"
-if [ -e "${out_file}.${t_ext}" ]; then
-	python "$exe_dir"/plot_2d.py "${out_file}.${t_ext}" -o "${plot_file}" \
-		-p "$prefix" -l "$tlen" -e "$energies_file"
-	if [ -e "${plot_file}" ]; then
-		open "${plot_file}"
-        cp "$plot_file" "$home_dir/Dropbox/Research/CCF_paper1/"
-    fi
-fi
-
-plot_file="${plot_root}_2Dccf.fits"
-detchans=$(python -c "import tools; print int(tools.get_key_val('${out_file}.fits', 0, 'DETCHANS'))")
-tlen2=$(( 2*tlen ))
-
-if [ -e "$out_dir/temp.dat" ]; then
-	fimgcreate bitpix=-32 \
-		naxes="${tlen2},${detchans}" \
-		datafile="$exe_dir/temp.dat" \
-		outfile="${plot_root}_2Dccf.fits" \
-		nskip=1 \
-		history=true \
-		clobber=yes
-else
-	echo -e "\tERROR: FIMGCREATE did not run. 2Dccf temp file does not exist."
-fi
-
-if [ -e "${plot_root}_2Dccf.fits" ]; then
-	echo "FITS 2D ccf ratio image: ${plot_root}_2Dccf.fits"
-else
-	echo -e "\tERROR: FIMGCREATE was not successful."
-fi
+#plot_file="${plot_root}_2Dccf.${p_ext}"
+#if [ -e "${out_file}.${t_ext}" ]; then
+#	python "$exe_dir"/plot_2d.py "${out_file}.${t_ext}" -o "${plot_file}" \
+#		-p "$prefix" -l "$tlen" -e "$energies_file"
+#	if [ -e "${plot_file}" ]; then
+#		open "${plot_file}"
+#        cp "$plot_file" "$home_dir/Dropbox/Research/CCF_paper1/"
+#    fi
+#fi
+#
+#plot_file="${plot_root}_2Dccf.fits"
+#detchans=$(python -c "import tools; print int(tools.get_key_val('${out_file}.fits', 0, 'DETCHANS'))")
+#tlen2=$(( 2*tlen ))
+#
+#if [ -e "$out_dir/temp.dat" ]; then
+#	fimgcreate bitpix=-32 \
+#		naxes="${tlen2},${detchans}" \
+#		datafile="$exe_dir/temp.dat" \
+#		outfile="${plot_root}_2Dccf.fits" \
+#		nskip=1 \
+#		history=true \
+#		clobber=yes
+#else
+#	echo -e "\tERROR: FIMGCREATE did not run. 2Dccf temp file does not exist."
+#fi
+#
+#if [ -e "${plot_root}_2Dccf.fits" ]; then
+#	echo "FITS 2D ccf ratio image: ${plot_root}_2Dccf.fits"
+#else
+#	echo -e "\tERROR: FIMGCREATE was not successful."
+#fi
 
 #####################
 ## Plotting the lags
