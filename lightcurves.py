2eedc8f42eb8ce8c4ebf23f9872c799384b87143
Abigail Stevens <A.L.Stevens@uva.nl>
2015-11-10 15:47:45 +0100

    Moved Lightcurves class from boostrap_multi_ccf and NormPSD class from ccf into a new file.

---
+++ lightcurves.py
@@ -0,0 +1,40 @@
+"""
+Class for CCF Lightcurves object and normalized power spectra.
+"""
+import numpy as np
+
+__author__ = 'Abigail Stevens <A.L.Stevens at uva.nl>'
+__year__ = "2015"
+
+
+class Lightcurves(object):
+    def __init__(self, n_bins=8192, detchans=64, type='ci'):
+        self.type = type
+
+        if type.lower() == "ci":
+            self.power_array = np.zeros((n_bins, detchans, 1), dtype=np.float64)
+            self.power = np.zeros((n_bins, detchans), dtype=np.float64)
+            self.pos_power = np.zeros((n_bins/2+1, detchans), dtype=np.float64)
+            self.mean_rate_array = np.zeros((detchans, 1), dtype=np.float64)
+            self.mean_rate = np.zeros(detchans)
+        else:
+            self.power_array = np.zeros((n_bins, 1), dtype=np.float64)
+            self.power = np.zeros(n_bins, dtype=np.float64)
+            self.pos_power = np.zeros(n_bins/2+1, dtype=np.float64)
+            self.mean_rate_array = 0.0
+            self.mean_rate = 0.0
+
+
+class NormPSD(object):
+    def __init__(self, n_bins=8192, detchans=64, type='ci'):
+        self.type = type
+        self.noise = 0.0
+
+        if type.lower() == "ci":
+            self.power = np.zeros((n_bins, detchans), dtype=np.float64)
+            self.variance = np.zeros(detchans, dtype=np.float64)
+            self.rms = np.zeros(detchans, dtype=np.float64)
+        else:
+            self.power = np.zeros(n_bins, dtype=np.float64)
+            self.variance = 0.0
+            self.rms = 0.0
